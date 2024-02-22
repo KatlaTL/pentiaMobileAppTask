98a6dca280aa025628181c9e126ded52ff0842ae
Asger <regsa13@gmail.com>
2024-02-22 14:48:57 +0100

    added activityIndicator when the app is loading

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,38 +1,44 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import SignIn from "./signin/SignIn";
 import useAuthStatus from "../hooks/useAuthStatus";
 import SignOut from "./signin/SingOut";
 import Register from "./signin/Register";
 import { globalStyle } from "../styles/global";
 
-const Main = (): React.JSX.Element | null => {
+const Main = (): React.JSX.Element => {
     const [isNewUser, setIsNewUser] = useState<boolean>(false);
     const { user, initializing } = useAuthStatus();
 
     if (initializing) {
-        console.log("initializing...")
-        return null;
-    }
-
-    if (!user) {
         return (
-            <View style={globalStyle.flex1}>
-                {isNewUser ? (
-                    <Register signInClick={() => setIsNewUser(false)} />
-                ) : (
-                    <SignIn registerClick={() => setIsNewUser(true)} />
-                )}
-            </View>
-        )
+            <View style={{
+                flex: 1,
+                justifyContent: 'center'
+            }} >
+                <ActivityIndicator size={100} color="#0000ff" />
+            </View >
+        );
     }
 
+if (!user) {
     return (
         <View style={globalStyle.flex1}>
-            <Text>YOU ARE SIGNED IN</Text>
-            <SignOut signOutClick={() => setIsNewUser(false)} />
+            {isNewUser ? (
+                <Register signInClick={() => setIsNewUser(false)} />
+            ) : (
+                <SignIn registerClick={() => setIsNewUser(true)} />
+            )}
         </View>
     )
+}
+
+return (
+    <View style={globalStyle.flex1}>
+        <Text>YOU ARE SIGNED IN</Text>
+        <SignOut signOutClick={() => setIsNewUser(false)} />
+    </View>
+)
 }
 
 export default Main;
