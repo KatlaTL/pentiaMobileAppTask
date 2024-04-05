0a4663ed9298282c7ccc335064385574e87ab2fe
Asger <regsa13@gmail.com>
2024-04-05 13:24:46 +0200

    added LogBox to hide warning from third party

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+// Ignore log notification by message
+LogBox.ignoreLogs([
+    // Exact message
+    'If you want to use Reanimated 2 then go through our installation steps',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
