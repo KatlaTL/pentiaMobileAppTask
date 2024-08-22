f01a78b06c84b0e26c5591a2ce4d849c451cecda
Asger Thorsboe Lundblad <asger@Asgers-MacBook-Pro.local>
2024-08-22 11:48:11 +0200

    removed obsolete useState

--- app/screens/chat-rooms/chat-rooms.screen.tsx
+++ app/screens/chat-rooms/chat-rooms.screen.tsx
@@ -1,13 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "../../redux/store/store";
-import { ChatRoomListType, fetchChatRoomList, selectChatRoomList } from "../../redux/reducers/chatRoomListSlice";
+import { fetchChatRoomList, selectChatRoomList } from "../../redux/reducers/chatRoomListSlice";
 import { ChatRoomsNavigationProps } from "../../navigators/app.navigator";
 import { ChatRoonsPresentation } from "./_components/chat-rooms-presentation";
 
 
 const ChatRoomsScreen = ({ navigation }: ChatRoomsNavigationProps): React.JSX.Element => {
-    const [chatRooms, setChatRooms] = useState<ChatRoomListType[]>([]);
     const [refreshing, setRefreshing] = useState<boolean>(false);
     const appDispatch = useAppDispatch();
     const chatRoomList = useSelector(selectChatRoomList);
@@ -21,12 +20,10 @@ const ChatRoomsScreen = ({ navigation }: ChatRoomsNavigationProps): React.JSX.El
 
     useEffect(() => getRoomList(), []);
 
-    useEffect(() => setChatRooms(chatRoomList), [chatRoomList]);
-
     const handleRoomClick = (chat_id: string, chat_name: string) => navigation.navigate("Chat", { chat_id: chat_id, chat_name: chat_name });
 
     return <ChatRoonsPresentation
-        chatRooms={chatRooms}
+        chatRooms={chatRoomList}
         getRoomList={getRoomList}
         handleRoomClick={handleRoomClick}
         refreshing={refreshing}
