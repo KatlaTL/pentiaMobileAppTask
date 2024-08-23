7de4f58c3ed1afeb05b76b3cb6d2dc5dc0e87d46
Asger Thorsboe Lundblad <asger@Asgers-MacBook-Pro.local>
2024-08-23 10:35:38 +0200

    removed unnecessary warning suppression

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ import { AppRegistry, LogBox } from 'react-native';
 import App from './app/App';
 import { name as appName } from './app.json';
 
-// Ignore log notification by message
-LogBox.ignoreLogs([
-    // Exact message
-    'If you want to use Reanimated 2 then go through our installation steps',
-]);
-
 AppRegistry.registerComponent(appName, () => App);
