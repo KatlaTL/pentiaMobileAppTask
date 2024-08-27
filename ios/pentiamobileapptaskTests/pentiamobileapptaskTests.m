3ee9bfcce9b3987d044194dce41f0278cc2f6e57
Asger Thorsboe Lundblad <asger@Asgers-MacBook-Pro.local>
2024-08-27 14:37:05 +0200

    renamed project to get ride of build error on IOS

--- ios/pentiamobileapptaskTests/pentiamobileapptaskTests.m
+++ ios/pentiamobileapptaskTests/pentiamobileapptaskTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface simpleReactNativeChattingAppTests : XCTestCase
+@interface pentiamobileapptaskTests : XCTestCase
 
 @end
 
-@implementation simpleReactNativeChattingAppTests
+@implementation pentiamobileapptaskTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
